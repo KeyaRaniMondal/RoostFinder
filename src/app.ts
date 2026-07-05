d1ef23544b0fefd620a89ea3977fe10e85240902
KeyaRaniMondal <keyamondal549@gmail.com>
2026-07-05 21:06:22 +0600

    completed basic project setup

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express,{Application} from 'express'
+import {Request,Response} from 'express'
+const app:Application=express()
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('Hello, World!')
+})
+export default app
