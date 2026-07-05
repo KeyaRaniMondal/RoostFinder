b6120bc5a9c33e73a985ea4e6dea91c4a5ddcdd5
KeyaRaniMondal <keyamondal549@gmail.com>
2026-07-05 22:07:12 +0600

    created middleware and prisma connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import express,{Application} from 'express'
 import {Request,Response} from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app:Application=express()
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin:'config.app_url',
+    credentials:true
+}))
 
 app.get('/',(req:Request,res:Response)=>{
     res.send('Hello, World!')
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import app from "./app"
+import config from "./config"
+import {prisma} from './lib/prisma'
 
-async function main(){
-    const port=5000
-    try{
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+async function main() {
+    const port = config.PORT || 5000
+    try {
+        await prisma.$connect()
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
     }
-    catch(error){
-console.log(error)
-process.exit(1)
+    catch (error) {
+        await prisma.$disconnect()
+        console.log(error)
+        process.exit(1)
     }
 }
 main()
