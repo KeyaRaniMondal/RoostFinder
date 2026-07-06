d5924fe4cbe6abbd4130e0b57d7487ffd2725b32
KeyaRaniMondal <keyamondal549@gmail.com>
2026-07-06 13:37:53 +0600

    added jwt authentication along with access & refresh token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { LoginUser } from "./auth.interface";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 
 const loginUser = async (payload: LoginUser) => {
     const { email, password } = payload
@@ -15,8 +18,71 @@ const loginUser = async (payload: LoginUser) => {
     if (!isPasswordMatched) {
         throw new Error("password is incorrect");
     }
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.JWT_ACCESS_SECRET!,
+        config.JWT_ACCESS_EXPIRES_IN as SignOptions
+    );
+
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.JWT_REFRESH_SECRET!,
+        config.JWT_REFRESH_EXPIRES_IN as SignOptions
+    );
+
+    return {
+        accessToken,
+        refreshToken
+    };
 }
 
+const refreshToken = async (refreshToken: string) => {
+    const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.JWT_REFRESH_SECRET!);
+
+    if (!verifiedRefreshToken.success) {
+        throw new Error(verifiedRefreshToken.error)
+    }
+
+    const { id } = verifiedRefreshToken.data as JwtPayload;
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            id
+        }
+    })
+
+    if (user.activeStatus === "BLOCKED") {
+        throw new Error("User is blocked!")
+    }
+
+    const jwtPayload = {
+        id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.JWT_ACCESS_SECRET!,
+        config.JWT_ACCESS_EXPIRES_IN as SignOptions
+    );
+
+    return { accessToken }
+}
+
+
 export const authService = {
-    loginUser
+    loginUser,
+    refreshToken
 }
